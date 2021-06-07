a24343e47952ad217c334107765bc63df8267fe9
Simon Breneis <simon.breneis@gmx.at>
2021-06-07 12:16:31 +0200

    Made SignatureTree Bidirectional
    SignatureTree so far has always only considered signature indices where a dimension was appended to existing signature indices. This is inherently asymmetric and got cleared up in this version. Now, also signature indices where a dimension is prepended to existing signature indices are considered. Remark: The class SignatureTreeLinear is not yet fully functional. Do not use it.

--- main.py
+++ main.py
@@ -76,6 +76,9 @@ def get_average_accuracy_rf(train_features, train_labels, test_features, test_la
 class SignatureTree:
     def __init__(self, train_paths, validation_paths, train_labels, validation_labels, initial_signature_level):
         # A family of paths is given as [ [ [path1dim1value1, path1dim1value2, ...], [path1dim2value1, ...], ...], ...]
+        self.train_paths = train_paths
+        self.validation_paths = validation_paths
+        self.test_paths = None
         self.train_increments = train_paths[:, :, 1:] - train_paths[:, :, :-1]
         self.validation_increments = validation_paths[:, :, 1:] - validation_paths[:, :, :-1]
         self.test_increments = None
@@ -83,12 +86,15 @@ class SignatureTree:
         self.validation_labels = validation_labels
         self.test_labels = None
         self.path_dim = train_paths.shape[1]
-        self.path_len = train_paths.shape[2]
         self.signature_nodes = []
-        self.signature_nodes.append(SignatureNode(train_values=np.ones(shape=(train_paths.shape[0], self.path_len)),
+        self.signature_leaves = []
+        self.signature_nodes.append(SignatureNode(train_values=np.ones(shape=(train_paths.shape[0], train_paths.shape[2])),
                                                   validation_values=np.ones(
-                                                      shape=(validation_paths.shape[0], self.path_len)),
-                                                  root=self, predecessor=None, index=[], is_active=True))
+                                                      shape=(validation_paths.shape[0], validation_paths.shape[2])),
+                                                  root=self, index=[], is_active=True, is_right_successor=True))
+        self.signature_nodes[0].compute_successors()
+        self.signature_leaves.extend(self.signature_nodes[0].get_successor_leaves())
+
         for signature_node in self.signature_nodes:
             if signature_node.get_level() < initial_signature_level:
                 signature_node.compute_successors()
@@ -97,11 +103,25 @@ class SignatureTree:
                     self.add_signature_node(successor)
 
     def add_signature_node(self, signature_node):
-        self.signature_nodes.append(signature_node)
-        signature_node.set_is_active(True)
+        if signature_node.get_index() not in self.get_indices():
+            self.signature_nodes.append(signature_node)
+            signature_node.compute_successors()
+            signature_node.set_is_active(True)
+            if signature_node in self.signature_leaves:
+                self.signature_leaves.remove(signature_node)
+            self.signature_leaves.extend(signature_node.get_successor_leaves())
 
     def get_path_dim(self):
         return self.path_dim
+
+    def get_train_values(self, i):
+        return self.train_paths[:, i, :]
+
+    def get_validation_values(self, i):
+        return self.validation_paths[:, i, :]
+
+    def get_test_values(self, i):
+        return self.test_paths[:, i, :]
 
     def get_train_increments(self, i):
         return self.train_increments[:, i, :]
@@ -120,6 +140,63 @@ class SignatureTree:
         for signature_node in self.signature_nodes:
             indices.append(signature_node.get_index())
         return indices
+
+    def get_signature_node_and_leave_indices(self):
+        known_indices = []
+        for signature_node in self.signature_nodes:
+            known_indices.append(signature_node.get_index())
+        for signature_leave in self.signature_leaves:
+            known_indices.append(signature_leave.get_index())
+        return known_indices
+
+    def get_number_samples(self, kind="train"):
+        if kind == "train":
+            return self.train_increments.shape[0]
+        if kind == "validation":
+            return self.validation_increments.shape[0]
+        if self.test_increments is None:
+            return 0
+        return self.test_increments.shape[0]
+
+    def get_normalized_features(self, kind="train"):
+        number_nodes = self.get_number_nodes()
+        features = np.empty(shape=(self.get_number_samples(kind), number_nodes))
+        for i in range(0, number_nodes):
+            features[:, i] = self.signature_nodes[i].get_normalized_features(kind)
+        return features
+
+    def find_new_node(self, number_rf=1, number_trees=100):
+        number_nodes = self.get_number_nodes() + 1
+        train_features = np.empty(shape=(self.get_number_samples("train"), number_nodes))
+        validation_features = np.empty(shape=(self.get_number_samples("validation"), number_nodes))
+        train_features[:, :-1] = self.get_normalized_features("train")
+        validation_features[:, :-1] = self.get_normalized_features("validation")
+        signature_leaves = self.signature_leaves
+        average_accuracies = np.empty(shape=(len(signature_leaves)))
+        for leave_nr in range(0, len(signature_leaves)):
+            train_features[:, -1] = signature_leaves[leave_nr].get_normalized_features("train")
+            validation_features[:, -1] = signature_leaves[leave_nr].get_normalized_features("validation")
+            average_accuracies[leave_nr] = get_average_accuracy_rf(train_features=train_features, train_labels=self.train_labels, test_features=validation_features, test_labels=self.validation_labels, number_rf=number_rf, number_trees=number_trees)
+        best_leave_nr = np.argmax(average_accuracies)
+        self.add_signature_node(signature_leaves[best_leave_nr])
+
+    def find_nodes_number(self, number_nodes=2, number_rf=1, number_trees=100):
+        for i in range(0, number_nodes):
+            self.find_new_node(number_rf=number_rf, number_trees=number_trees)
+
+    def get_accuracy(self, test_paths, test_labels, number_rf=1, number_trees=100):
+        self.test_paths = test_paths
+        self.test_increments = test_paths[:, :, 1:] - test_paths[:, :, :-1]
+        self.test_labels = test_labels
+        self.signature_nodes[0].compute_test_values(test_values=np.ones(shape=(test_paths.shape[0], test_paths.shape[2])))
+        return get_average_accuracy_rf(train_features=self.get_normalized_features("train"), train_labels=self.train_labels, test_features=self.get_normalized_features("test"), test_labels=self.test_labels, number_rf=number_rf, number_trees=number_trees)
+
+
+class SignatureTreeLinear(SignatureTree):
+
+    def add_signature_node(self, signature_node):
+        self.signature_nodes.append(signature_node)
+        signature_node.set_is_active(True)
 
     def get_signature_leaves(self):
         signature_leaves = []
@@ -169,36 +246,56 @@ class SignatureTree:
 
 
 class SignatureNode:
-    def __init__(self, train_values, validation_values, root, predecessor, index, is_active):
+    def __init__(self, train_values, validation_values, root, index, is_active, is_right_successor):
         self.train_values = train_values
         self.validation_values = validation_values
         self.test_values = None
+        self.train_increments = train_values[:, 1:] - train_values[:, :-1]
+        self.validation_increments = validation_values[:, 1:] - validation_values[:, :-1]
+        self.test_increments = None
         self.root = root
         self.is_active = is_active
-        self.predecessor = predecessor
         self.successors = []
         self.successors_computed = False
         self.index = index
+        self.is_right_successor = is_right_successor
 
     def compute_successors(self):
         if not self.successors_computed:
+            known_indices = self.root.get_signature_node_and_leave_indices()
             for i in range(0, self.root.get_path_dim()):
-                train_values_successor = integrate_against_indefinite(self.train_values, self.root.get_train_increments(i))
-                validation_values_successor = integrate_against_indefinite(self.validation_values, self.root.get_validation_increments(i))
                 index_successor = self.index.copy()
-                index_successor.append(i+1)
-                self.successors.append(SignatureNode(train_values=train_values_successor,
-                                                     validation_values=validation_values_successor,
-                                                     root=self.root, predecessor=self,
-                                                     index=index_successor, is_active=False))
+                index_successor.append(i + 1)
+                if index_successor not in known_indices:
+                    train_values_successor = integrate_against_indefinite(self.train_values, self.root.get_train_increments(i))
+                    validation_values_successor = integrate_against_indefinite(self.validation_values, self.root.get_validation_increments(i))
+                    self.successors.append(SignatureNode(train_values=train_values_successor,
+                                                         validation_values=validation_values_successor,
+                                                         root=self.root, index=index_successor, is_active=False,
+                                                         is_right_successor=True))
+                index_successor = self.index.copy()
+                index_successor.insert(0, i+1)
+                is_this_index_symmetric = [x for x in index_successor if x != (i+1)]
+                if index_successor not in known_indices and len(is_this_index_symmetric) != 0:
+                    train_values_successor = integrate_against_indefinite(self.root.get_train_values(i), self.train_increments)
+                    validation_values_successor = integrate_against_indefinite(self.root.get_validation_values(i), self.validation_increments)
+                    self.successors.append(SignatureNode(train_values=train_values_successor,
+                                                         validation_values=validation_values_successor,
+                                                         root=self.root, index=index_successor, is_active=False,
+                                                         is_right_successor=False))
         self.successors_computed = True
 
     def compute_test_values(self, test_values):
         self.test_values = test_values
+        self.test_increments = test_values[:, 1:] - test_values[:, :-1]
         for successor in self.successors:
             if successor.get_is_active():
-                test_values_successor = integrate_against_indefinite(self.test_values, self.root.get_test_increments(successor.get_index()[-1] - 1))
-                successor.compute_test_values(test_values_successor)
+                if successor.get_is_right_successor():
+                    test_values_successor = integrate_against_indefinite(self.test_values, self.root.get_test_increments(successor.get_index()[-1] - 1))
+                    successor.compute_test_values(test_values_successor)
+                else:
+                    test_values_successor = integrate_against_indefinite(self.root.get_test_values(successor.get_index()[0] - 1), self.test_increments)
+                    successor.compute_test_values(test_values_successor)
 
     def get_level(self):
         return len(self.index)
@@ -211,6 +308,9 @@ class SignatureNode:
 
     def set_is_active(self, is_active):
         self.is_active = is_active
+
+    def get_is_right_successor(self):
+        return self.is_right_successor
 
     def get_successors_computed(self):
         return self.successors_computed
@@ -236,25 +336,10 @@ class SignatureNode:
         return successor_leaves
 
 
-'''
-train_paths = np.array([[[0., 0.5, 1., 1.5], [0., 1., 2., 3.]],
-                        [[0., 0.5, 1., 1.5], [0., 5., -1., -2.]]])
-
-validation_paths = np.array([[[0., 0.5, 1., 1.5], [0., 1., 2., 3.]],
-                        [[0., 0.5, 1., 1.5], [0., 5., -1., -2.]]])
-
-test_paths = np.array([[[0., 0.5, 1., 1.5], [0., 1., 2., -10.]],
-                        [[0., 0.5, 1., 1.5], [0., 5., -1., 20.]]])
-
-train_labels = np.array([True, False])
-validation_labels = np.array([True, False])
-test_labels = np.array([True, False])
-'''
-
 number_train_paths = 2000
 number_validation_paths = 2000
 number_test_paths = 2000
-time_steps = 1000
+time_steps = 100
 T = 1.
 
 train_paths = np.empty(shape=(number_train_paths, 2, time_steps+1))
@@ -277,7 +362,9 @@ for i in range(0, number_test_paths):
     test_paths[i, :, :] = brownian_time(T, time_steps).transpose()
     test_labels[i] = check_hitting(test_paths[i, 1, :], 1.)
 
-tree = SignatureTree(train_paths=train_paths, validation_paths=validation_paths, train_labels=train_labels, validation_labels=validation_labels, initial_signature_level=2)
-tree.find_nodes_number(number_nodes=2, number_rf=10)
+tree = SignatureTree(train_paths=train_paths, validation_paths=validation_paths, train_labels=train_labels, validation_labels=validation_labels, initial_signature_level=0)
+tree.find_nodes_number(number_nodes=10, number_rf=1)
 print(tree.get_indices())
+print(len(tree.get_indices()))
+print(tree.get_signature_node_and_leave_indices())
 print(tree.get_accuracy(test_paths=test_paths, test_labels=test_labels))
