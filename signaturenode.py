Minor bugfix regarding scaling of features

signaturenode.py
@@ -1,14 +1,14 @@
+import numpy as np
 import utilities as util
-from sklearn.preprocessing import StandardScaler
 
 
 class SignatureNode:
-    def __init__(self, train_values, val_values, root, index, is_active, is_right_successor):
-        self.train_values = train_values
-        self.val_values = val_values
-        self.test_values = None
-        self.train_increments = train_values[:, 1:] - train_values[:, :-1]
-        self.val_increments = val_values[:, 1:] - val_values[:, :-1]
+    def __init__(self, train_paths, val_paths, root, index, is_active, is_right_successor):
+        self.train_paths = train_paths
+        self.val_paths = val_paths
+        self.test_paths = None
+        self.train_increments = train_paths[:, 1:] - train_paths[:, :-1]
+        self.val_increments = val_paths[:, 1:] - val_paths[:, :-1]
         self.test_increments = None
         self.root = root
         self.is_active = is_active
@@ -16,59 +16,56 @@ class SignatureNode:
         self.successors_computed = False
         self.index = index
         self.is_right_successor = is_right_successor
+        self.mu = np.average(train_paths[:, -1])
+        self.sigma = np.std(train_paths[:, -1])
 
     def compute_successors(self):
         if not self.successors_computed:
             known_indices = self.root.node_and_leave_indices()
-            for i in range(0, self.root.path_dim()):
+            for i in range(self.root.path_dim()):
                 index_successor = self.index.copy()
                 index_successor.append(i + 1)
                 if index_successor not in known_indices:
-                    train_values_successor = util.integrate_against_indefinite(self.train_values,
-                                                                               self.root.get_train_increments(i))
-                    val_values_successor = util.integrate_against_indefinite(self.val_values,
-                                                                             self.root.get_val_increments(i))
+                    train_values_successor = util.integrate(self.train_paths, self.root.get_train_increments(i))
+                    val_values_successor = util.integrate(self.val_paths, self.root.get_val_increments(i))
                     self.successors.append(SignatureNode(train_values_successor, val_values_successor, self.root,
                                                          index_successor, is_active=False, is_right_successor=True))
                 index_successor = self.index.copy()
                 index_successor.insert(0, i + 1)
                 is_this_index_symmetric = [x for x in index_successor if x != (i + 1)]
                 if index_successor not in known_indices and len(is_this_index_symmetric) != 0:
-                    train_values_successor = util.integrate_against_indefinite(self.root.get_train_paths(i),
-                                                                               self.train_increments)
-                    val_values_successor = util.integrate_against_indefinite(self.root.get_val_paths(i),
-                                                                             self.val_increments)
+                    train_values_successor = util.integrate(self.root.get_train_paths(i), self.train_increments)
+                    val_values_successor = util.integrate(self.root.get_val_paths(i), self.val_increments)
                     self.successors.append(SignatureNode(train_values_successor, val_values_successor, self.root,
                                                          index_successor, is_active=False, is_right_successor=False))
         self.successors_computed = True
 
-    def compute_test_values(self, test_values):
-        self.test_values = test_values
-        self.test_increments = test_values[:, 1:] - test_values[:, :-1]
+    def compute_test_paths(self, test_paths):
+        self.test_paths = test_paths
+        self.test_increments = test_paths[:, 1:] - test_paths[:, :-1]
         for successor in self.successors:
             if successor.is_active:
                 if successor.is_right_successor:
-                    test_values_successor = util.integrate_against_indefinite(self.test_values,
-                                                                              self.root.get_test_increments(
-                                                                                  successor.index[-1] - 1))
-                    successor.compute_test_values(test_values_successor)
+                    test_values_successor = util.integrate(self.test_paths,
+                                                           self.root.get_test_increments(successor.index[-1] - 1))
+                    successor.compute_test_paths(test_values_successor)
                 else:
-                    test_values_successor = util.integrate_against_indefinite(
+                    test_values_successor = util.integrate(
                         self.root.get_test_paths(successor.index[0] - 1), self.test_increments)
-                    successor.compute_test_values(test_values_successor)
+                    successor.compute_test_paths(test_values_successor)
 
     def level(self):
         return len(self.index)
 
-    def raw_features(self, kind="train"):
+    def raw_features(self, kind):
         if kind == "train":
-            return self.train_values[:, -1]
+            return self.train_paths[:, -1]
         if kind == "validation":
-            return self.val_values[:, -1]
-        return self.test_values[:, -1]
+            return self.val_paths[:, -1]
+        return self.test_paths[:, -1]
 
-    def normalized_features(self, kind="train"):
-        return StandardScaler().fit_transform(self.raw_features(kind).reshape(-1, 1)).flatten()
+    def normalized_features(self, kind):
+        return (self.raw_features(kind)-self.mu)/(self.sigma if self.sigma != 0. else 1.)
 
     def successor_leaves(self):
         successor_leaves = []

signaturetree.py
@@ -19,8 +19,8 @@ class SignatureTree:
         self.n_pred = n_pred
         self.nodes = []
         self.leaves = []
-        self.nodes.append(sn.SignatureNode(train_values=np.ones(shape=(train_paths.shape[0], train_paths.shape[2])),
-                                           val_values=np.ones(shape=(val_paths.shape[0], val_paths.shape[2])),
+        self.nodes.append(sn.SignatureNode(train_paths=np.ones(shape=(train_paths.shape[0], train_paths.shape[2])),
+                                           val_paths=np.ones(shape=(val_paths.shape[0], val_paths.shape[2])),
                                            root=self, index=[], is_active=True, is_right_successor=True))
         self.nodes[0].compute_successors()
         self.leaves.extend(self.nodes[0].successor_leaves())
@@ -183,10 +183,8 @@ class SignatureTree:
                     self.training_active = False
             return True
         for i in range(n_nodes):
-            print(i)
             found_new_node = False
             while not found_new_node:
-                print(f"Finding new node among {len(self.leaves)} leaves.")
                 if len(self.leaves) != 0:
                     found_new_node = self.find_new_node(mode)
                 else:
@@ -197,7 +195,7 @@ class SignatureTree:
     def predictions(self, test_paths):
         self.test_paths = test_paths
         self.test_increments = test_paths[:, :, 1:] - test_paths[:, :, :-1]
-        self.nodes[0].compute_test_values(test_values=np.ones(shape=(test_paths.shape[0], test_paths.shape[2])))
+        self.nodes[0].compute_test_paths(test_paths=np.ones(shape=(test_paths.shape[0], test_paths.shape[2])))
         return np.array([self.predictor(self.features("train"), self.train_labels, self.features("test"))
                          for _ in range(self.n_pred)])
 

utilities.py
@@ -15,9 +15,9 @@ def brownian_motion(T, N):
 
 
 def brownian_time(T, N):
-    time = np.array([i * T / N for i in range(0, N + 1)]).reshape(N + 1, 1)
+    t = np.array([i * T / N for i in range(0, N + 1)]).reshape(N + 1, 1)
     brownian = brownian_motion(T, N).reshape(N + 1, 1)
-    return np.concatenate((time, brownian), axis=1).transpose()
+    return np.concatenate((t, brownian), axis=1).transpose()
 
 
 def brownian_motion_multidimensional(T, N, dim):
@@ -28,7 +28,7 @@ def check_hitting(path, threshold):
     return np.amax(path) >= threshold
 
 
-def integrate_against_indefinite(x, y_diff):
+def integrate(x, y_diff):
     """
     Computes the indefinite integral int x dy.
     :param x: Vector of x-values
@@ -47,8 +47,8 @@ def check_hitting_levy(paths):
     y = paths[:, 1, :]
     x_diff = x[:, 1:] - x[:, :-1]
     y_diff = y[:, 1:] - y[:, :-1]
-    levy_area = integrate_against_indefinite(x, y_diff) - integrate_against_indefinite(y, x_diff)
-    return integrate_against_indefinite(x, y_diff)[:, -1] + x[:, -1] > 0
+    levy_area = integrate(x, y_diff) - integrate(y, x_diff)
+    return integrate(x, y_diff)[:, -1] + x[:, -1] > 0
 
 
 def predictor_nn(train_features, train_labels, test_features):